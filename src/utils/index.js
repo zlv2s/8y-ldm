40527a6196fe9e760d0d896680a1a767bd2a9257
zlv2s <706809256@qq.com>
2019-12-23 18:18:56 +0800

    fix pvTj

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
+// *empty Object KeyValue
 export function emptyObj(...obj) {
   obj.forEach(item => {
     Object.keys(item).forEach(key => {
@@ -6,6 +7,7 @@ export function emptyObj(...obj) {
   })
 }
 
+// *select text
 export function selectText(element) {
   let text = document.getElementById(element)
   if (document.body.createTextRange) {
@@ -26,6 +28,7 @@ export function selectText(element) {
   }
 }
 
+// *monthMap
 export const dateMap = {
   '01': 'JAN',
   '02': 'FEB',
@@ -39,4 +42,59 @@ export const dateMap = {
   '10': 'OCT',
   '11': 'NOV',
   '12': 'DEC'
+}
+
+// *cookie
+let caution = false
+export function setCookie(name, value, expires, path, domain, secure) {
+  let curCookie =
+    name +
+    '=' +
+    escape(value) +
+    (expires ? ';expires=' + expires.toGMTString() : '') +
+    (path ? '; path=' + path : '') +
+    (domain ? '; domain=' + domain : '') +
+    (secure ? ';secure' : '')
+  if (!caution || (name + '=' + escape(value)).length <= 4000) {
+    document.cookie = curCookie
+  } else if (confirm('Cookie exceeds 4KB and will be cut!')) {
+    document.cookie = curCookie
+  }
+}
+
+export function getCookie(name) {
+  let prefix = name + '='
+  let cookieStartIndex = document.cookie.indexOf(prefix)
+  if (cookieStartIndex === -1) {
+    return null
+  }
+  let cookieEndIndex = document.cookie.indexOf(
+    ';',
+    cookieStartIndex + prefix.length
+  )
+  if (cookieEndIndex === -1) {
+    cookieEndIndex = document.cookie.length
+  }
+  return unescape(
+    document.cookie.substring(cookieStartIndex + prefix.length, cookieEndIndex)
+  )
+}
+
+export function deleteCookie(name, path, domain) {
+  if (getCookie(name)) {
+    document.cookie =
+      name +
+      '=' +
+      (path ? '; path=' + path : '') +
+      (domain ? '; domain=' + domain : '') +
+      '; expires=Thu, 01-Jan-70 00:00:01 GMT'
+  }
+}
+
+export function fixDate(date) {
+  let base = new Date(0)
+  let skew = base.getTime()
+  if (skew > 0) {
+    date.setTime(date.getTime() - skew)
+  }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="outer-container">
     <a-row>
       <a-col :span="24">
-          <Header />
+        <Header />
       </a-col>
     </a-row>
     <a-row>
@@ -11,17 +11,44 @@
         <Board />
       </a-col>
       <a-col :xs="0" :lg="8">
-       <a-card :bordered="false" size="small">
-         <h2>Tools</h2>
+        <a-card :bordered="false" size="small">
+          <h2>Tools</h2>
           <ul>
             <li><a href="/weather">Terminal Aerodrome Forecasts (TAFs)</a></li>
-            <li><a href="https://www.flightradar24.com/" target="_blank">Flightradar24</a></li>
-            <li><a href="https://flightadsb.variflight.com/" target="_blank">Flight ADSB</a></li>
-            <li><a href="https://www.aircn.org/toolbox/metar.php" target="_blank">机场气象报文解析</a></li>
-            <li><a href="https://www.planespotters.net/search?" target="_blank">Aircraft Information Inquiry</a></li>
-            <li><a href="https://www.airfleets.net/recherche/search.htm" target="_blank">Aircraft Information Inquiry(2)</a></li>
-        </ul>
-       </a-card>
+            <li>
+              <a href="https://www.flightradar24.com/" target="_blank"
+                >Flightradar24</a
+              >
+            </li>
+            <li>
+              <a href="https://flightadsb.variflight.com/" target="_blank"
+                >Flight ADSB</a
+              >
+            </li>
+            <li>
+              <a href="https://www.aircn.org/toolbox/metar.php" target="_blank"
+                >机场气象报文解析</a
+              >
+            </li>
+            <li>
+              <a href="https://www.planespotters.net/search?" target="_blank"
+                >Aircraft Information Inquiry</a
+              >
+            </li>
+            <li>
+              <a
+                href="https://www.airfleets.net/recherche/search.htm"
+                target="_blank"
+                >Aircraft Information Inquiry(2)</a
+              >
+            </li>
+          </ul>
+        </a-card>
+      </a-col>
+    </a-row>
+    <a-row>
+      <a-col :span="24">
+        <p class="info" v-html="info"></p>
       </a-col>
     </a-row>
   </div>
@@ -31,9 +58,15 @@
 import Board from '@/components/Board.vue'
 import Header from '@/components/Header.vue'
 import { getFltStatus } from '@/api'
+import { getCookie, setCookie, fixDate } from '@/utils'
 
 export default {
   name: 'home',
+  data() {
+    return {
+      info: ''
+    }
+  },
   components: {
     Header,
     Board
@@ -45,6 +78,22 @@ export default {
         // todo
       }
     })
+    this.pvTj()
+  },
+  methods: {
+    pvTj() {
+      let now = new Date()
+      fixDate(now)
+      now.setTime(now.getTime() + 365 * 24 * 60 * 60 * 1000)
+      let visits = getCookie('counter')
+      if (!visits) {
+        visits = 1
+      } else {
+        visits = parseInt(visits) + 1
+      }
+      setCookie('counter', visits, now)
+      this.info = `您是到访的第${visits}位用户！`
+    }
   }
 }
 </script>
@@ -52,5 +101,9 @@ export default {
 <style lang="scss" scoped>
 .outer-container {
   padding: 8px;
+  .info {
+    text-align: center;
+    font-size: 12px;
+  }
 }
 </style>
